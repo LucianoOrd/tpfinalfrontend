ae63ef3950555c07606854944d85ade65fd5017f
Nixomart <nixoma1@gmail.com>
2023-06-24 00:25:36 -0300

    changesss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormularioAlumnoComponent } from './components/formulario-alumno/formulario-alumno.component';
-import { ListaAlumnosComponent } from './components/lista-alumnos/lista-alumnos.component';
-
+import { FormularioAlumnoComponent } from './components/administrador/formulario-alumno/formulario-alumno.component';
+import { ListaAlumnosComponent } from './components/administrador/lista-alumnos/lista-alumnos.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -15,19 +14,12 @@ import { HomeComponent } from './components/home/home.component';
 @NgModule({
   declarations: [
     AppComponent,
-
     FormularioAlumnoComponent,
-    ListaAlumnosComponent
-
+    ListaAlumnosComponent,
     LoginComponent,
-<<<<<<< HEAD
     SignupComponent,
     NavbarComponent,
     HomeComponent
-=======
-    SignupComponent
-
->>>>>>> bf7b1a473d206f7ede282d7632c4a8c253a495a8
   ],
   imports: [
     BrowserModule,
