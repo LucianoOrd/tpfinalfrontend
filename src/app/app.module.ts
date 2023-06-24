9ada6fd80c28d03613d34ec5937c877d1c0dccc9
IgnacioGareca156 <nachosan9@gmail.com>
2023-06-23 23:06:53 -0300

    creacion de carpeta administrativo y creacion de componentes lista-alumnos y form-alumnos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormularioAlumnoComponent } from './components/formulario-alumno/formulario-alumno.component';
+import { ListaAlumnosComponent } from './components/lista-alumnos/lista-alumnos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioAlumnoComponent,
+    ListaAlumnosComponent
   ],
   imports: [
     BrowserModule,
