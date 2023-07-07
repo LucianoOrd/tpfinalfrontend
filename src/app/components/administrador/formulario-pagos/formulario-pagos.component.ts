24a065a82ae4a8b18b0ff864563f042a27b9380c
Luciano Ordoñez <lucianordonez123@gmail.com>
2023-07-07 17:15:34 -0300

    agregado formulario de pago

--- src/app/components/administrador/formulario-pagos/formulario-pagos.component.ts
+++ src/app/components/administrador/formulario-pagos/formulario-pagos.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { InsumoService } from 'src/app/services/insumo.service';
+import { Insumo } from 'src/app/models/insumo';
+import { Pago } from 'src/app/models/pago';
+
 
 @Component({
   selector: 'app-formulario-pagos',
   templateUrl: './formulario-pagos.component.html',
   styleUrls: ['./formulario-pagos.component.css']
 })
-export class FormularioPagosComponent implements OnInit{
-constructor(){ }
 
-ngOnInit(): void {
+export class FormularioPagosComponent implements OnInit {
 
-}
+  pago!: Pago;
+  insumo!: Insumo;
+  arrayInsumos!: Array<Insumo>;
+
+  constructor(private pagoInsumoService: InsumoService) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  obtenerInsumos() {
+    this.pagoInsumoService.getInsumos().subscribe(
+      (result) => {
+        this.arrayInsumos = new Array<Insumo>();
+        var aux: Insumo = new Insumo();
+        result.forEach((insumo: Insumo) => {
+          Object.assign(aux, insumo);
+          this.arrayInsumos.push(aux);
+          aux = new Insumo();
+        });
+      },
+      (error) => { console.log(error); }
+    )
+  }
+
+  calcularTotal(){
+    
+  }
 
 }
